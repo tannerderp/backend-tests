127ce1974475173819dacb0fafa437a6e9e145db
Tannerderp <tannersingleton9@gmail.com>
2019-05-28 22:18:21 +0000

    route params

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -9,6 +9,13 @@ app.get("/bye", function(req, res){
 app.get("/dog", function(req, res){
     res.send("Your mom");
 })
+app.get("/ebola/:anything", function(req, res){
+    var thing = req.params.anything;
+    res.send("Ebola is bad, and it's related to "+thing);
+})
+app.get("*", function(req, res){
+    res.send("404 your mom not found");
+})
 app.listen(9000, undefined, function(){
     console.log("server has started on port 9000");
 });
