1451f446218511603efbc2b7eeac51cc05a22e1c
Tannerderp <tannersingleton9@gmail.com>
2019-06-03 21:40:28 +0000

    EJS: conditionals and loops

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -9,6 +9,16 @@ app.get("/fallinlovewith/:thing", function(req, res){
         thingVar: thing,
     });
 })
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "post 1", author: "Dingus"},
+        {title: "I'm screwed", author: "Geromison"},
+        {title: "Anyone selling memes", author: "johny boy"},
+    ];
+    res.render("posts.ejs", {
+        posts: posts,
+    });
+})
 app.listen(9000, undefined, function(){
     console.log("server has started on port 9000");
 });
