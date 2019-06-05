f1866cb1641ab64c10faa9a2c838eff2e951321a
Tannerderp <tannersingleton9@gmail.com>
2019-06-05 22:45:46 +0000

    Serving custom assets

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 })
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {
+    res.render("love", {
         thingVar: thing,
     });
 })
@@ -15,7 +17,7 @@ app.get("/posts", function(req, res){
         {title: "I'm screwed", author: "Geromison"},
         {title: "Anyone selling memes", author: "johny boy"},
     ];
-    res.render("posts.ejs", {
+    res.render("posts", {
         posts: posts,
     });
 })
