4e7eb99d8dfcffdb5aec25afc8c01745a325ca30
Tannerderp <tannersingleton9@gmail.com>
2019-06-10 17:39:42 +0000

    sunset time API example

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,12 +1,9 @@
 var request = require("request");
-request("https://www.tannerderp.tk/games/p5/basketball", function(error, response, body){
-    if(error){
-        console.log("epic error:");
-        console.log(error);
-    } else{
-        if(response.statusCode == 200){
-            //it worked
-            console.log(body);
-        }
+//yahoo sunset api is apparently gone, so I just found anotha one
+console.log("sunset in new york is...")
+request("https://api.sunrise-sunset.org/json?lat=40.7128&lng=74.0060", function(error, response, body){ //this should be new york
+    if(!error&&response.statusCode == 200){
+        var parsedData = JSON.parse(body);
+        console.log(parsedData["results"]["sunset"]);
     }
 })
